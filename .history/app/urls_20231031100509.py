0cc29267325544960cfa8a13cc099ab11730fac8
AFOR <foussejolie02@gmail.com>
2023-11-01 18:16:13 +0100

    first commit

---
+++ .history/app/urls_20231031100509.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+    path("", views.connection, name="connection"),
+    path("index", views.index, name="index"),
+    path('formulairemanuscrit/',views.formulairemanuscrit,name='formulairemanuscrit'),
+
+
+    ]
+
+
