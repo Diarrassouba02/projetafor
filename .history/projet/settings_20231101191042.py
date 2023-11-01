042aa3fd400f04c5e7d8e53e1ebac7fc33119672
AFOR <foussejolie02@gmail.com>
2023-11-01 19:16:35 +0100

    first commit

--- .history/projet/settings_20231101191042.py
+++ .history/projet/settings_20231101191042.py
@@ -127,8 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 STATICFILES_DIRS = (
