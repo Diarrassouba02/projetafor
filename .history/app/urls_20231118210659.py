f8556d543e9a77665f9447b451c43c187f3c13c5
AFOR <foussejolie02@gmail.com>
2023-11-20 11:26:38 +0100

    first commit

---
+++ .history/app/urls_20231118210659.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+    path("", views.connection, name="connection"),
+    path("index", views.index, name="index"),
+    path('formulairemanuscrit/',views.formulairemanuscrit,name='formulairemanuscrit'),
+    path("datah", views.datatables, name="datah"),
+    path('logout', views.logout_view, name='logout'),
+    path('<str:pk>', views.detaille, name='detail'),
+    path('<str:pk>', views.modifier, name='detail'),
+
+
+    ]
+
+
